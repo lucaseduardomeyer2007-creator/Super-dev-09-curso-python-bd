66f9ef28fca42fa834689fdd66f056e11081d244
Lucas <lucaseduardomeyer2007@gmail.com>
2026-08-17 18:12:08 -0300

    Curso

---
+++ src/clientes.py
@@ -0,0 +1,152 @@
+from datetime import date
+from mysql import connector
+
+
+HOST = "127.0.0.1"
+PORTA = 3306
+USUARIO = "root"
+SENHA = "admin"
+BANCO = "restau_calabresa"
+
+def conectar():
+   
+    conexao = connector.connect(
+        host=HOST,
+        port=PORTA,
+        user=USUARIO,
+        password=SENHA,
+        database=BANCO,
+    )
+    return conexao
+    
+
+def cadastrar():
+    print("\n---- CADASTRAR CLIENTE----")
+    nome = input("Nome: ")
+    documento = input("documento: ")
+    telefone = int(input("Salário: ").replace (",", "."))
+    
+    
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT INTO clientes (nome, documento, telefone) VALUES (%s, %s, %s)",
+    (nome, documento, telefone),
+)
+
+
+    conexao.commit()
+    print(f"\n[OK] Cliente cadastrado com id: {cursor.lastrowid}")
+
+    cursor.close()
+    conexao.close()
+
+
+
+
+def listar_clientes():
+
+    conexao = conectar()
+  
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    SELECT
+        id, nome, documento, telefone, 
+    FROM clientes
+    ORDER BY nome ASC
+""")
+    
+    clientes = cursor.fetchall()
+
+    if len(clientes) == 0:
+        print("Nenhum cliente cadastrado")
+        return
+
+    print("-"*76, end="")
+    print (f"\n {'ID':<4} {'NOME':>25} {'DOCUMENTO':<20} {'TELEFONE':<12}")
+    print("="*76)
+    for cliente in clientes:
+        id = cliente[0]
+        nome = cliente[1]
+        documento = cliente[2] if cliente[2] else "Sem documento"
+        telefone = cliente[3] if cliente[3] else "Sem telefone"
+
+
+        print(
+            f"{id:<4} {nome:<25} {documento:<20} {telefone:<12}"
+        )
+    print("-"*76)
+
+
+def excluir_clientes():
+    listar_clientes()
+
+    id_cliente = int(input("Id do cliente que você quer excluir: "))
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("DELETE  FROM clientes WHERE id = %s", (id_cliente,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+    # Rowcount é a quantidade de linhas que foram afetadas
+    if cursor.rowcount == 0:
+        print("Cliente não encontrado com este id")
+    else:
+        print("Registro apagado com sucesso")
+
+
+def alterar_clientes():
+    listar_clientes()
+
+    id_cliente = int(input("ID do funcionário que você quer alterar: "))
+    nome = input("Nome: ")
+    documento = input("documento: ")
+    telefone = int(input("Cliente: ").replace (",", "."))
+
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute(
+        "UPDATE clientes SET nome = %s, documento = %s, telefone = %s WHERE id = %s",
+        (nome, documento, telefone, id_cliente)
+    )
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    if cursor.rowcount == 0:
+        print("Cliente não encontrado com este id")
+    else:
+        print("Registro alterado com sucesso")
+
+
+
+def menu_cliente():
+    mensagem = """MENU:
+    1 - Listar
+    2 - Cadastrar
+    3 - Editar
+    4 - Apagar
+    5 - Voltar
+    Digite a opção desejada:"""
+
+    opcao = int(input(mensagem))
+
+    while opcao != 5:
+        if opcao == 1:
+            listar_clientes()
+        elif opcao == 2:
+            cadastrar()
+        elif opcao == 3:
+            alterar_clientes()
+        elif opcao == 4:
+            excluir_clientes()
+        elif opcao != 5:
+            print("Opção inválida")
+            print("\n")
+
+        opcao = int(input(mensagem))
---
+++ src/pratos_feitos.py
@@ -0,0 +1,144 @@
+from datetime import date
+from mysql import connector
+
+HOST = "127.0.0.1"
+PORTA = 3306
+USUARIO = "root"
+SENHA = "admin"
+BANCO = "restau_calabresa"
+
+def conectar():
+    """Abre a conexão com MySQL e retorna ela"""
+    conexao = connector.connect(
+        host=HOST,
+        port=PORTA,
+        user=USUARIO,
+        password=SENHA,
+        database=BANCO,
+    )
+    return conexao
+
+def cadastrar():
+    print("\n---- CADASTRAR Pratos Feitos----")
+    nome = input("Nome do prato: ")
+    custo = float(input("Custo: ").replace (",", "."))    
+    
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT INTO pratos_feitos (nome, custo) VALUES (%s, %s)",
+    (nome, custo),
+)
+    
+    conexao.commit()
+    print(f"\n[OK] prato feito com id: {cursor.lastrowid}")
+
+    cursor.close()
+    conexao.close()
+
+
+def listar_pratos_feitos():
+    # Abrir a conexão com o banco de dados 
+    conexao = conectar()
+    # Criar o cursor para poder executar algum comando no banco de dados
+    cursor = conexao.cursor()
+    #Definir o comando de consulta dos funcionários
+    cursor.execute("""
+    SELECT
+        id, nome, custo
+    FROM pratos_feitos
+    ORDER BY nome ASC
+""")
+    # fetchall() retorna todas as linhas encontradas naquela consulta
+    # cada linha contém uma tupla com onde cada posição é a coluna do select
+    pratos_feitos = cursor.fetchall()
+
+    if len(pratos_feitos) == 0:
+        print("Nenhum prato cadastrado")
+        return
+
+    print("-"*76, end="")
+    print (f"\n {'ID':<4} {'NOME':>25} {'CUSTO':<20}")
+    print("="*76)
+    for prato in pratos_feitos:
+        id = prato[0]
+        nome = prato[1]
+        custo = prato[2]
+
+        print(
+            f"{id:<4} {nome:<25} {custo:<20}"
+        )
+    print("-"*76)
+
+
+def excluir_pratos_feitos():
+    listar_pratos_feitos()
+
+    id_pratos_feitos = int(input("Id do funcionario que você quer excluir: "))
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("DELETE  FROM pratos_feitos WHERE id = %s", (id_pratos_feitos,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+    # Rowcount é a quantidade de linhas que foram afetadas
+    if cursor.rowcount == 0:
+        print("Prato não encontrado com este id")
+    else:
+        print("Registro apagado com sucesso")
+
+
+def alterar_pratos_feitos():
+    listar_pratos_feitos()
+
+    id_pratos_feitos = int(input("ID do funcionário que você quer alterar: "))
+    nome = input("Nome: ")
+    
+    custo = float(input("Prato: ").replace (",", "."))
+    
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute(
+        "UPDATE pratos_feitos SET nome = %s, custo = %s WHERE id = %s",
+        (nome, custo, id_pratos_feitos)
+    )
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    if cursor.rowcount == 0:
+        print("Funcionário não encontrado com este id")
+    else:
+        print("Registro alterado com sucesso")
+
+
+
+def menu_prato_feito():
+    mensagem = """MENU:
+    1 - Listar
+    2 - Cadastrar
+    3 - Editar
+    4 - Apagar
+    5 - Voltar
+    Digite a opção desejada:"""
+
+    opcao = int(input(mensagem))
+
+    while opcao != 5:
+        if opcao == 1:
+            listar_pratos_feitos()
+        elif opcao == 2:
+            cadastrar()
+        elif opcao == 3:
+            alterar_pratos_feitos()
+        elif opcao == 4:
+            excluir_pratos_feitos()
+        elif opcao != 5:
+            print("Opção inválida")
+            print("\n")
+
+        opcao = int(input(mensagem))
